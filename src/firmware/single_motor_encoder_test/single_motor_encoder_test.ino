8566dbf8672dcaa43f734266a68cd8080d3bbd3b
Oshadha Gunasekara <oshadha123@gmail.com>
2023-07-19 19:09:44 -0700

    add single motor encoder test

---
+++ src/firmware/single_motor_encoder_test/single_motor_encoder_test.ino
@@ -0,0 +1,63 @@
+#define MOTOR_ENCA 3
+#define MOTOR_ENCB 2
+
+// from experimentation, seems to have ~680 counts per revolution
+volatile long enc_count = 0;
+
+// update count on ENCA change
+void handleEncAChange() {
+  int encA = digitalRead(MOTOR_ENCA);
+  int encB = digitalRead(MOTOR_ENCB);
+
+  if (encA == HIGH) {
+    if (encB == HIGH) {
+      enc_count += 1;
+    } else {
+      enc_count -= 1;
+    }
+  } else {
+    if (encB == HIGH) {
+      enc_count -= 1;
+    } else {
+      enc_count += 1;
+    }
+  }
+}
+
+// update count on ENCB change
+void handleEncBChange() {
+  int encA = digitalRead(MOTOR_ENCA);
+  int encB = digitalRead(MOTOR_ENCB);
+
+  if (encB == HIGH) {
+    if (encA == LOW) {
+      enc_count += 1;
+    } else {
+      enc_count -= 1;
+    }
+  } else {
+    if (encA == LOW) {
+      enc_count -= 1;
+    } else {
+      enc_count += 1;
+    }
+  }
+}
+
+void setup() {
+  // setup encoder pins
+  pinMode(MOTOR_ENCA, INPUT);
+  pinMode(MOTOR_ENCB, INPUT);
+  attachInterrupt(digitalPinToInterrupt(MOTOR_ENCA), handleEncAChange, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(MOTOR_ENCB), handleEncBChange, CHANGE);
+
+  // initialize serial and wait to open
+  Serial.begin(9600);
+  while (!Serial) {;}
+}
+
+void loop() {
+  // continuously print encoder value
+  Serial.println(enc_count);
+  delay(100);
+}
